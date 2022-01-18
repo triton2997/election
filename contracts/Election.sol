e65179b8e4e1e7541c3fded91fb6f986f9c9d705
Vignesh Subramanian <vignesh2997@gmail.com>
2022-01-18 22:14:43 +0530

    Added front end

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,28 @@
 pragma solidity ^0.5.16;
 
 contract Election {
+    
+    // Model the Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
 
-    string public candidate;
+    // Store and fetch Candidates
+    mapping(uint => Candidate) public candidates;
+
+    // Store Candidates count
+    uint public candidatesCount;
 
     // Constructor
     constructor () public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
